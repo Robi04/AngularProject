244d0852bbdf2a0debf4a7bb2e1f797cb63f6c54
Robin <robin73.bochu@gmail.com>
2023-12-16 03:13:45 +0100

    members list done

--- library-app/src/app/member/member.component.ts
+++ library-app/src/app/member/member.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './member.component.html',
   styleUrl: './member.component.css'
 })
-export class MemberComponent {
+export class LoanComponent {
 
 }
--- library-app/src/app/members-list/members-list.component.ts
+++ library-app/src/app/members-list/members-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MemberService } from '../member.service';
+import { Member } from '../models/member.model';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-members-list',
-  standalone: true,
-  imports: [],
   templateUrl: './members-list.component.html',
-  styleUrl: './members-list.component.css'
+  styleUrls: ['./members-list.component.css'],
+  standalone: true,
+  imports: [
+    HttpClientModule,CommonModule
+    // any other imports needed by this component
+  ],
+  providers: [MemberService] // Providing BookService if it's not already provided globally
 })
-export class MembersListComponent {
+
+
+export class MembersListComponent implements OnInit{
+  members: Member[];
+
+  constructor(private memberService: MemberService) { }
+
+  ngOnInit() {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.memberService.getMembers()
+      .subscribe(members => this.members = members)
+  }
 
 }
